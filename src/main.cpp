b2b3d9aace4eb8d2bd3507d389965f5036df2c3c
Duane Byer <duane.byer@gmail.com>
2017-01-22 13:42:10 -0800

    Added a basic CMake build setup with unit tests

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "tensor.h"
+#include "octree.h"
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "Hello world!\n";
+	return 0;
+}
+
---
+++ test/octree_test.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE OctreeTest
+
+#include "octree.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(PassTest) {
+	BOOST_CHECK_EQUAL(4, 2 * 2);
+}
+
