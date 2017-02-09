4214949bddedcb541903fac2874ec3efa019ddc5
Duane Byer <duane.byer@gmail.com>
2017-02-08 21:51:57 -0800

    Created a simple (unfailable) octree test

--- test/octree_test.cpp
+++ test/octree_test.cpp
@@ -1,10 +1,26 @@
 #define BOOST_TEST_MODULE OctreeTest
 
 #include "octree.h"
+#include "tensor.h"
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(PassTest) {
+using namespace nbody;
+
+struct Leaf {
+	int data;
+	Leaf(int data) : data(data) {
+	}
+};
+
+struct Node {
+	int data;
+	Node(int data = 0) : data(data) {
+	}
+};
+
+BOOST_AUTO_TEST_CASE(CreateOctree) {
+	Octree<Leaf, Node, 3> octree({0.0, 0.0, 0.0}, {1.0, 1.0, 1.0});
 	BOOST_CHECK_EQUAL(4, 2 * 2);
 }
 
