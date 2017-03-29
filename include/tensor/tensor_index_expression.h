d69e5b86ef422901732e97dd6eb8002cc0edf2f4
Duane Byer <duane.byer@gmail.com>
2017-03-29 09:31:50 -0700

    Changed the structure of the TensorIndexExpression class (template parameters and members)

--- include/tensor/tensor_index_expression.h
+++ include/tensor/tensor_index_expression.h
@@ -1,6 +1,8 @@
 #ifndef __NBODY_TENSOR_TENSOR_INDEX_EXPRESSION_H_
 #define __NBODY_TENSOR_TENSOR_INDEX_EXPRESSION_H_
 
+#include <algorithm>
+#include <iterator>
 #include <type_traits>
 
 #include "tensor.h"
@@ -8,6 +10,7 @@
 namespace nbody {
 
 enum class TensorIndex {
+	All,
 	A, B, C, D, E, F, G,
 	H, I, J, K, L, M, N, O, P,
 	Q, R, S, T, U, V,
@@ -17,28 +20,52 @@ enum class TensorIndex {
 
 template<
 	std::size_t N, std::size_t M, std::size_t Dim, typename Scalar,
-	std::size_t IndicesN, std::size_t IndicesM,
-	bool Const>
-class TensorIndexExpression final {
+	bool Const, bool IsRef>
+class TensorIndexReference final {
 	
 private:
 	
-	using TensorReference = std::conditional_t<
-		Const,
-		Tensor<N, M, Dim, Scalar> const&,
-		Tensor<N, M, Dim, Scalar>&>;
+	using Tensor = std::conditional_t<
+		IsRef,
+		std::conditional_t<
+			Const,
+			Tensor<N, M, Dim, Scalar> const&,
+			Tensor<N, M, Dim, Scalar>&>
+		Tensor<N, M, Dim, Scalar> >;
 	
-	TensorReference _tensor;
-	TensorIndex _upperIndices[IndicesN];
-	TensorIndex _lowerIndices[IndicesM];
+	Tensor _tensor;
+	TensorIndex _upperIndices[N];
+	TensorIndex _lowerIndices[M];
+	
+	TensorIndexExpression(
+			Tensor tensor,
+			TensorIndex upperIndices[N],
+			TensorIndex lowerIndices[M]) :
+			_tensor(tensor) {
+		std::copy(
+			std::begin(upperIndices),
+			std::end(upperIndices),
+			std::begin(_upperIndices));
+		std::copy(
+			std::begin(lowerIndices),
+			std::end(lowerIndices),
+			std::begin(_lowerIndices));
+	}
 	
 public:
+	
+	operator TensorIndexReference<N, M, Dim, Scalar, true, IsRef>() const {
+		return TensorIndexExpression<N, M, Dim, Scalar, true, Ref>(
+			_tensor,
+			_upperIndices,
+			_lowerIndices);
+	}
 	
 	template<typename Const_ = Const>
 	std::enable_if_t<Const_, void> operator=(
 			TensorIndexExpression<
 				N, M, Dim, Scalar,
-				IndicesN, IndicesM> const& rhs);
+				Const, IsRef> const& rhs) const;
 	
 };
 
